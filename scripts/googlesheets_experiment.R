56b096c81f868382fde0d4939b65039ac2d99d51
Nathan-Nguyen1 <nathannguyen801@gmail.com>
2023-02-19 15:39:23 -0700

    updating repo

--- scripts/googlesheets_experiment.R
+++ scripts/googlesheets_experiment.R
@@ -4,3 +4,11 @@ rm(list = ls()); gc()
 
 pacman::p_load(tidyverse, rio, here, googlesheets4)
 
+subjects <- c("ART", "BIO", "CHM", "SOC", "STP")
+
+for (i in subjects) {
+  print(i)
+}
+
+blah <- data.frame(neko = c("black", "white", "orange", "grey"),
+                   dog = c("brown", "black", "gold", "white"))
